051b68dd5209e2f95834f6e98e9258c52a2ef25f
diarm-glitch <diar.mustafa24@gmail.com>
2026-05-02 22:24:53 +0200

    Login API + JWT token

--- frontend/backend/controllers/auth.controller.js
+++ frontend/backend/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const db = require('../config/db');
 const bcrypt = require('bcrypt');
 
@@ -45,6 +46,46 @@ exports.register = async (req, res) => {
 });
 };
 
+
 exports.login = (req, res) => {
-    res.send('Login endpoint');
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({message: 'Email and password are reuqired'});
+    }
+
+    db.query('SELECT * FROM Users WHERE email = ?', [email], async (err, results) => {
+        if (err) {
+            return res.status(500).json({message: 'Database error', error: err});
+        }
+
+        if (results.length === 0) {
+            return res.status(400).json({message: 'Invalid email or password'});
+        }
+
+        const user = results[0];
+
+        const isMatch = await bcrypt.compare(password, user.password_hash);
+
+        if(!isMatch) {
+            return res.status(400).json({message:'Invalid email or password'});
+        }
+
+        const token = jwt.sign(
+            { id: user.id, email:user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: '1h'}
+        );
+
+        res.status(200).json ({
+            message:'Login successful',
+            token:token,
+            user: {
+                id:user.id,
+                emri:user.emri,
+                mbiemri:user.mbiemri,
+                email:user.email
+            }
+        });
+    });
 };
