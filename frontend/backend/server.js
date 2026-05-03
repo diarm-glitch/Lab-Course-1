98b04909932cb03bf7049312b73a5354dbc8b493
diarm-glitch <diar.mustafa24@gmail.com>
2026-05-03 02:15:23 +0200

    Vehicle CRUD

--- frontend/backend/server.js
+++ frontend/backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const db = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
+const customerRoutes = require('./routes/customer.routes');
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+
+app.use('/customers', customerRoutes);
+
 
 app.get('/', (req, res) => {
     res.send('API is running');
